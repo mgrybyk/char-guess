341ea9a53841142ea03b2ac0caf01bfdb4f3d7d8
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-11-19 14:24:57 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Guesser from './components/Guesser/Guesser.vue'
+</script>
+
+<template>
+  <main>
+    <div class="mx-auto max-w-screen-md container">
+      <Guesser />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Guesser/Guesser.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div>Guesser</div>
+</template>
+
+<style scoped></style>
